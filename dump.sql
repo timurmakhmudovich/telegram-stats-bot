1cab959e99c787aabda39cc2ef352f97a3e803bf
timurmakhmudovich <46539493+timurmakhmudovich@users.noreply.github.com>
2019-08-22 19:25:31 +0300

    Update dump.sql

--- dump.sql
+++ dump.sql
@@ -3,7 +3,7 @@ CREATE DATABASE db_name;
 GRANT ALL PRIVILEGES ON DATABASE db_name TO db_username;
 
 CREATE TABLE activity (
-	username VARCHAR(30),
+    username VARCHAR(30),
     day DATE,
     messages INT
 );
